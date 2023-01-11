7dd66e0d15d0a5e94d13f9368d90fa6a9f686956
artemii258 <Artem2014l2top@yandex.ru>
2023-01-11 21:56:30 +0500

    update files

--- server.mjs
+++ server.mjs
@@ -5,9 +5,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.use(express.static(`${__dirname}/dist`));
-app.listen(port, function () {
-	console.log(`Сервер запущен на порту ${port}`);
+app.use(express.static(`${__dirname}/dist`)); // если использовать __dirname без всех манипуляций сверху, то выскакивает ошибка ReferenceError: __dirname is not defined in ES module scope
+
+app.listen(process.env.PORT || PORT, function () {
+	console.log(`Сервер запущен на порту ${PORT}`);
 });
